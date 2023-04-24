4e7ced9bd2d6301e47c3a988b05f805b7cbc70a4
Alby1 <alby.macchi2004@gmail.com>
2023-04-24 09:44:50 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pandas import DataFrame
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
 
 def get_database():
- 
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
    CONNECTION_STRING = "mongodb://localhost/"
  
@@ -12,16 +16,20 @@ def get_database():
  
    # Create the database for our example (we will use the same database throughout the tutorial
    return client['US']
+
+   
   
-# This is added so that many files can reuse the function get_database()
-if __name__ == "__main__":   
-  
-   # Get the database
+@app.get("/")
+async def root():
    dbname = get_database()
 
    cities = dbname['US']
    cc = cities.find().limit(5)
-   cc = DataFrame(cc)
-   print(cc[0]['city'])
+   #cc = DataFrame(cc)
+   print(cc)
+   return cc
+  
+if __name__ == "__main__":   
+   uvicorn.run("main:app", port=7300, reload=True)
    
    
