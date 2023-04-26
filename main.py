cc6254020316cfb09e192c0a5cb2bcdd18920e70
Alby1 <alby.macchi2004@gmail.com>
2023-04-26 09:19:37 +0200

    boh il progetto ciao

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from bson import ObjectId
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pandas import DataFrame
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import uvicorn
+import re
 
 app = FastAPI()
 
@@ -19,15 +22,31 @@ def get_database():
 
    
   
-@app.get("/")
-async def root():
+@app.get("/lettere")
+async def lettere(mittente: str = None):
    dbname = get_database()
 
+   lettere = dbname['lettere']
    cities = dbname['US']
-   cc = cities.find().limit(5)
+   cc = lettere.find()
+   if mittente is not None:
+      m = re.compile(f".*{mittente}*.", re.IGNORECASE)
+      cc = lettere.find({"mittente": m})
    #cc = DataFrame(cc)
-   print(cc)
-   return cc
+   ccc = []
+   for c in cc:
+      c["_id"] = str(c["_id"])
+      c['arrivo']['citta'] = cities.find_one({"_id" : ObjectId(c['arrivo']['citta'])})
+      c['arrivo']['citta']["_id"] = str(c['arrivo']['citta']["_id"])
+      c['partenza']['citta'] = cities.find_one({"_id" : ObjectId(c['partenza']['citta'])})
+      c['partenza']['citta']["_id"] = str(c['partenza']['citta']["_id"])
+      ccc.append(c)
+   return ccc
+
+
+@app.get("/")
+async def root():
+   return FileResponse("index.html")
   
 if __name__ == "__main__":   
    uvicorn.run("main:app", port=7300, reload=True)
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/Alby1/piffy-humongous-letterepazze
